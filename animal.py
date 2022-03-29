94dd0deed7023d5821ebbdf049dca1c54ed2594a
JunDeBus <debusj@oregonstate.edu>
2022-03-28 23:22:26 -0400

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,8 +1,8 @@
 '''
-Name: Jun DeBus
-GitHub Username: JunDeBus
+Author: Jun DeBus
+GitHub username: JunDeBus
 Date: 3/29/2022
-
+Description: Program asks for your favorite animal and returns a string stating what your favorite animal is.
 '''
 
 fave_animal = input("What is your favorite animal? ")
