abec8015db929b3bf9b502cf243cb0edc290e407
JunDeBus <debusj@oregonstate.edu>
2022-03-28 23:09:57 -0400

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,9 @@
+'''
+Name: Jun DeBus
+GitHub Username: JunDeBus
+Date: 3/29/2022
+
+'''
+
+fave_animal = input("What is your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
