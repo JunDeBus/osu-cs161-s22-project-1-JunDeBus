786af83b55de33265d9c4fe95ddcbc53b387ab6c
JunDeBus <debusj@oregonstate.edu>
2022-03-28 23:11:51 -0400

    Update animal.py

--- animal.py
+++ animal.py
@@ -5,5 +5,5 @@ Date: 3/29/2022
 
 '''
 
-fave_animal = input("What is your favorite animal: ")
+fave_animal = input("What is your favorite animal? ")
 print("Your favorite animal is the " + fave_animal + ".")
