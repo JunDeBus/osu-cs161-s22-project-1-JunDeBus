96e1467195c1383f21720dd88f2301937d2f008a
JunDeBus <debusj@oregonstate.edu>
2022-03-30 16:16:55 -0400

    Update animal.py

--- animal.py
+++ animal.py
@@ -5,5 +5,5 @@ Date: 3/30/2022
 Description: Program asks for your favorite animal and returns a string stating what your favorite animal is.
 '''
 
-fave_animal = input("What is your favorite animal? ")
+fave_animal = input("What is your favorite animal: ")
 print("Your favorite animal is the " + fave_animal + ".")
